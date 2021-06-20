571c509f9e2c7e3602309ad616fd3ebe5957af05
mayank-s-832 <mayank.s.832@gmail.com>
2021-06-20 22:42:24 +0530

    Object repository- page classes

--- WeatherReportingComparator_Demo/src/main/java/com/comparator/qa/pages/NDTVHomePage.java
+++ WeatherReportingComparator_Demo/src/main/java/com/comparator/qa/pages/NDTVHomePage.java
@@ -1,10 +1,27 @@
 package com.comparator.qa.pages;
 
-public class NDTVHomePage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import com.comparator.qa.base.BaseClass;
 
+public class NDTVHomePage extends BaseClass {
+	
+	@FindBy(xpath = "//a[@id ='h_sub_menu']")
+	WebElement expandButton;
+
+	@FindBy(xpath = "//a[contains(text(),'WEATHER')]")
+	WebElement weatherPageLink;
+	
+	public NDTVHomePage() {
+		PageFactory.initElements(driver, this);
 	}
-
+	
+	
+	public NDTVWeatherPage clickOnWeatherLink() {
+		expandButton.click();
+		weatherPageLink.click();
+		return new NDTVWeatherPage();
+	}
 }
--- WeatherReportingComparator_Demo/src/main/java/com/comparator/qa/pages/NDTVWeatherPage.java
+++ WeatherReportingComparator_Demo/src/main/java/com/comparator/qa/pages/NDTVWeatherPage.java
@@ -1,10 +1,36 @@
 package com.comparator.qa.pages;
 
-public class NDTVWeatherPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import com.comparator.qa.base.BaseClass;
 
+public class NDTVWeatherPage extends BaseClass {
+
+	@FindBy(xpath = "//input[@id = 'searchBox']")
+	WebElement citySearchBox;
+	
+	@FindBy(xpath = "//b[contains(text(),'Temp in Degrees')]")
+	WebElement getTempinDegree;
+	
+	public NDTVWeatherPage() {
+		PageFactory.initElements(driver, this);
 	}
-
+	
+	public WebElement checkCityCheckbox(String cityname) {
+		String checkboxXpath = "//input[@id = '"+cityname+"']"; 
+		System.out.println(checkboxXpath);
+		WebElement element =driver.findElement(By.xpath(checkboxXpath));
+		return element;
+		
+	}
+	
+//	public static void main(String[] args) {
+//		NDTVWeatherPage obj = new NDTVWeatherPage();
+//		obj.checkCityCheckbox("Ajmer");
+//
+//	}
 }
+
