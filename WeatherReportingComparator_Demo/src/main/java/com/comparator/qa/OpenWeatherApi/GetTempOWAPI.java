49e1fdc3ada794d612fc05315a395ee4cf4a52cf
mayank-s-832 <mayank.s.832@gmail.com>
2021-06-23 00:58:13 +0530

    created class to get temp froom API response

--- WeatherReportingComparator_Demo/src/main/java/com/comparator/qa/OpenWeatherApi/GetTempOWAPI.java
+++ WeatherReportingComparator_Demo/src/main/java/com/comparator/qa/OpenWeatherApi/GetTempOWAPI.java
@@ -1,10 +1,57 @@
 package com.comparator.qa.OpenWeatherApi;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
 import com.comparator.qa.base.BaseClass;
 
-public class GetTempOWAPI extends BaseClass {
-	
-	
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
-	
+public class GetTempOWAPI extends BaseClass {
+
+	public static void main(String[] args) {
+		try {
+			new GetTempOWAPI().getTempfromOpenWeather("Mumbai", "metric", "7fe67bf08c80ded756e598d6f8fedaea");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public float getTempfromOpenWeather(String city, String unitValue, String key) throws IOException {
+
+		RestAssured.baseURI = prop.getProperty("baseURI");
+		Response response = null;
+		HashMap<String, Float> tempdetails = new HashMap<String, Float>();
+		float currentTemp = 0.00f;
+
+		RequestSpecification request = RestAssured.given();
+		try {
+			response = request.log().all().queryParam("q", city).queryParam("units", unitValue).queryParam("appid", key)
+					.when().get("/weather");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Response :" + response.asString());
+		System.out.println("Status Code :" + response.getStatusCode());
+
+		// Initializing JsonPath object
+		JsonPath jsonobj = response.jsonPath();
+		tempdetails = jsonobj.get("main");
+		for (Entry<String, Float> e : tempdetails.entrySet()) {
+			// System.out.println(e.getKey()+" "+e.getValue());
+			if (e.getKey().equals("temp")) {
+				currentTemp = e.getValue();
+			}
+		}
+		System.out.println("API: " + currentTemp);
+		return currentTemp;
+
+	}
+
 }
